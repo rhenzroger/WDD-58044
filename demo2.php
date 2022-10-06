39fd76ad194832a5b5c3f2389f71dd3c91854f2b
Martinez, Rhenz Roger E <70188184+rhenzroger@users.noreply.github.com>
2022-10-06 21:52:32 +0800

    Add files via upload

---
+++ demo2.php
@@ -0,0 +1,80 @@
+<html>
+
+	<head>
+		<title> My First Web page </title>
+
+	</head>
+
+	<body>
+		<?php
+		$content = "Welcome to PHP Programming";
+		$a = 5;
+		$x = 8;
+		$y = 8;
+		$b = "Hi ";
+		$c = "again";
+		echo "<br>";
+		$_msg = 'Hello again';
+		define("value",100);
+		//case-sensitive echo value;
+		echo "<br>";
+		echo $content;
+		echo "<br>";
+		echo "<br>".$a;
+		echo "<br>".$_msg;
+		//Single line comment
+		/* Multi-line comment */
+		//Arithmetic operations
+		echo "<br>";
+		echo $x%$y. "\n";
+		echo "<br>";
+		if($x===$y)
+		{
+			echo "They are Identical". "\n";
+		}
+		else
+		{
+			echo "They are not Identical". "\n";
+		}
+		
+		echo "<br>";
+		echo $b.=$c;
+		
+		
+		//Looping statement
+		echo "<br>";
+		
+		//for loop
+		for($a=5; $a>=1; $a--)
+		{
+			echo $a."\n";
+		}
+		
+		//Looping statement
+		echo "<br>";
+		
+		//while loop
+		$a = 5;
+		while($a>=1)
+		{
+				echo $a."\n";
+				$a--;
+		}
+		
+		//Looping statement
+		echo "<br>";
+		
+		//do... while loop
+		$a = 5;
+		do{
+			echo $a."\n";
+			$a--;
+		}while($a>=1);
+		?>
+		</body>
+
+
+
+
+
+</html>
