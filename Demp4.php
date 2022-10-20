53c3e7ef6d44aade98458cd46f3572c69747f1ad
Martinez, Rhenz Roger E <70188184+rhenzroger@users.noreply.github.com>
2022-10-20 12:00:04 +0800

    Add files via upload

---
+++ Demp4.php
@@ -0,0 +1,41 @@
+<?php
+	$fuel = 1; 
+	//while loop
+	while ($fuel>=1)
+	{
+		echo "There's enough fuel <br>"; 
+		$fuel=sfuel-1:
+	}
+	
+	echo "<br>";*/ 
+	//do.. while 
+	/*do
+		echo "$count times 12 is" . $count . 12 . "<br>"; 
+	while(++$countc=12);*/
+
+//for loop
+	/*for (scount=1:$count<=12:++$count)
+	{
+		echo "Scount time 12 is" . $count *12 . "<br>";
+	}*/
+	
+	for ($x 0:$x<10:5x++)
+	{
+			if(x==4)
+			{
+					break: 
+			}
+		echo "$". "<br>";
+	}
+//Application 1
+
+	
+		for ($i=1; $i<=5; $i++)
+			
+		{  	
+		echo "The hello is: ".$i."<br/>";
+			
+		} 
+
+
+?>
